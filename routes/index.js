d03757bb7970a320078b9136ae51729354c1dabc
NessCollege <nesscollege2017@gmail.com>
2017-08-11 10:56:43 +0300

    db

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+//DB Import:
+var { Pool, Client } = require('pg')
+
+
+//DB Global variables:
+//this variables are known only in this module. (index.js)
+var constring = 'postgresql://postgres:123@localhost:5432/postgres'
+var pool = new Pool({connectionString : constring});
+
 /* GET form page. */
 router.get('/', function(req, res) {
   res.render('index', {title: 'Welcome home!'});
@@ -12,9 +21,38 @@ router.post("/", function(req, res){
   res.json(bodyParams);
 })
 
-router.get('/api', function(req, res){
-  res.json({result: 'ApiResult'});
+router.get('/api/students', function(req, res){
+  pool.query("SELECT * FROM Students", [], function(err, dbResult){
+    if(err){
+      return res.render('error', {message:err.message, error:err})
+    }
+    else{
+      var rows = dbResult.rows;
+      return res.json(rows)
+    }
+  })
 })
+
+router.get('/api/addStudent', function(req, res){
+  var name = req.query.name;
+  var lastname = req.query.lastname;
+
+  if(name && lastname){
+
+    var SQL = "INSERT INTO Students(name, lastname) VALUES($1, $2)";
+     pool.query(SQL,[name, lastname], function(err, result){
+       if(err){
+         return res.json(err);
+       }else{
+         return res.json({message:"success"});
+       }
+     })
+  }else{
+    return res.json({err:'bad request'});
+  }
+
+ 
+});
 
 router.get('/hodor', function(req, res) {
   var q = req.query;
